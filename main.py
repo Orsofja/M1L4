4e2a09e4f1854b9a555d613b6f289fb7f6384263
Orsofja <156570803+Orsofja@users.noreply.github.com>
2024-02-04 10:06:05 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from bot_logic import gen_pass
+import discord
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith('!password'):
+        await message.channel.send(gen_pass(8))
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTIwMTA2Nzg5ODA4OTc2Njk5Mg.GYecVZ.LzK--jISWNnZWSPsB05LzsroHcmkPXnuBv1Rgk")
