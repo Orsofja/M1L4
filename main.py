8f1d757c0d5b06560c34935af6aee4ac9ac3a10b
Orsofja <156570803+Orsofja@users.noreply.github.com>
2024-02-09 18:11:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,15 +20,8 @@ async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
 @bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
+async def repeat(ctx, times: int, content='repeating...'):
+    for i in range(times):
+        await ctx.send(content)
 
 bot.run("TOKEN")
